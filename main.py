a6c4b25c45ba291efe5a5b68d1ed189df2d98f27
eledelaf <eledelaf@ucm.es>
2026-01-26 22:41:16 +0000

    Core Sales Performance Analysis

--- main.py
+++ main.py
@@ -9,3 +9,15 @@ data = "Sample_data.xlsx"
 df = pd.read_excel(data)
 
 print(df.head())
+print(df.columns)
+
+# Key questions to answer
+# Total revenue (revenue = units sold x Price per unit)
+df['Revenue'] = df['Units Sold'] * df['Sale Price']
+total_revenue = df['Revenue'].sum()
+print("The total revenue is :", total_revenue)
+
+# Total Units Sold
+print("The total units sold are:", int(df['Units Sold'].sum()))
+
+# Average Order Value
