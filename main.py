c0e5724719b9e5dea40f2981b007f7e4d4dd9f29
eledelaf <eledelaf@ucm.es>
2026-01-26 21:32:42 +0000

    Create enviroment

---
+++ main.py
@@ -0,0 +1,11 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib
+import seaborn as sns
+
+# Load data
+data = "/Users/elenadelafuente/Desktop/Proyectos programación/Power-BI-and-Profit-Prediction"
+df = pd.read_excel(data)
+
+df.head()
